ef2639519689305a2325051416edf165fd7a8039
Yuri <dog-pit10@yandex.ru>
2021-01-10 18:48:51 +0300

    Commit 5: created file Message.tsx.

---
+++ src/components/Dialogs/Message/Message.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import s from './../Dialogs.module.css'
+
+type PropsMessageType = {
+    message: string
+}
+export const Message = (props: PropsMessageType) => {
+    return (
+        <div className={s.message}>
+            {props.message}
+        </div>
+    )
+}
